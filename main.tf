8328298a5a00ff9a44a2c61a7539defbe4aecb69
mohamedezz96 <mohamed.ahmed.ezzeldeen@gmail.com>
2022-04-03 12:57:22 +0200

    ll

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
- resource "aws_vpc" "Main" {                # Creating VPC here.
+ resource "aws_vpc" "Main" {                # Creating VPC here
    cidr_block       = var.main_vpc_cidr     # Defining the CIDR block use 10.0.0.0/24 for demo
    instance_tenancy = "default"
  }
