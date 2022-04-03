da43fdece53f6e1389912e68bcb15c83f2a879a4
mohamedezz96 <mohamed.ahmed.ezzeldeen@gmail.com>
2022-04-03 13:33:05 +0200

    final

--- provider.tf
+++ provider.tf
@@ -1,3 +1,15 @@
 provider "aws" {
    region = "us-east-2"
  }
+
+
+
+terraform {
+  backend "s3" {
+    bucket = "team2-project-bucket"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+
