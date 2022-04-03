5d9266e0fe225f843de65c4fa63e53a9ec33044e
mohamedezz96 <mohamed.ahmed.ezzeldeen@gmail.com>
2022-04-03 12:29:20 +0200

    third

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@
  }
 
 
- resource "aws_internet_gateway" "IGW" {    # Creating Internet Gateway
-    vpc_id =  aws_vpc.Main.id               # vpc_id will be generated after we create VPC
+ resource "aws_internet_gateway" "IGW" {    # Creating Internet Gateway.
+    vpc_id =  aws_vpc.Main.id               # vpc_id will be generated after we create VPC.
  }
 
 
